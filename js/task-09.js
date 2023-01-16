d9db621ac4d5a69b572b36e926eaa09e59323af8
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-01-16 18:17:55 +0200

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+// Отримуємо доступ до body кнопки зміни кольору та спану з майбутнім значенням кольору
+const bodyEl = document.querySelector('body');
+const colorValueText = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+
+  bodyEl.style.backgroundColor = randomColor;
+
+  colorValueText.textContent = randomColor;
+}
+
+changeColorBtn.addEventListener('click', changeBackgroundColor);
