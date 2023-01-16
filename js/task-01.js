cb3f09e859029eb6dbca16e059c5e76717753bf3
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-01-16 14:31:23 +0200

    task-1 done

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+// Шукаємо в DOM список категорій та їх елементів
+const categories = document.querySelector("#categories");
+const items = document.querySelectorAll(".item");
+
+// виводимо в консоль кількість категорій
+console.log("Number of categories: ", categories.children.length);
+
+// виводимо у консоль порожній рядок(тут і далі для візуальної відповідності ТЗ)
+console.log("");
+
+// виводимо у консоль заголовки категорій та кількість елементів для кожної
+items.forEach((item) => {
+  console.log("Category: ", item.firstElementChild.textContent);
+  console.log("Elements: ", item.lastElementChild.children.length);
+  console.log("");
+});
