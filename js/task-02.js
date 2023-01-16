891901e535e07bea37df5948f08fea1d3a01a316
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-01-16 14:46:20 +0200

    task-2 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+//  Отримуємо доступ до порожнього списку інгредієнтів
+const listOfIngredients = document.querySelector("#ingredients");
+
+// Створюємо і додаємо до списку інгредієнти на основі масиву
+
+ingredients.map((element) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = element;
+  ingredient.classList.add("item");
+  listOfIngredients.append(ingredient);
+});
