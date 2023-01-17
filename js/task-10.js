eb34398396f2838d1499968fc4037ae446313f35
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-01-17 10:31:27 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,21 +1,33 @@
+// Отримуємо доступ до інпута, кнопок та контейнера для боксів
 const input = document.querySelector('input');
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxesContainer = document.querySelector('#boxes');
+
+// створюємо змінні для зберігання числового значення мінімальної та максимальної кількості боксів
+//  які введені в інпут
 const minBoxesAmount = Number(input.min);
 const maxBoxesAmount = Number(input.max);
 
+// додаємо слухача події input. Значенням колбека буде слухач події на кнопці createBtn
 input.addEventListener('input', function () {
   createBtn.addEventListener('click', createBoxes);
 });
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+// колбек-функція для слухача події кнопки createBtn
 function createBoxes(amount) {
+  // створюємо порожній масив для подальшого додавання боксів
   const boxesArray = [];
+
+  //  присвоюємо параметру amount поточне значення в інпуті
   amount = input.value;
 
+  // за допомогою циклу for створюємо на кожній ітерації div(бокс), стилізуємо його
+  //  і додаємо в масив boxesArray
   for (let i = 0; i < amount; i += 1) {
     if (amount <= maxBoxesAmount && amount >= minBoxesAmount) {
       const boxEl = document.createElement('div');
@@ -30,12 +42,15 @@ function createBoxes(amount) {
     }
   }
 
+  // додаємо масив з боксами у контейнер
   boxesContainer.append(...boxesArray);
 }
 
+// створюємо функцію для очищення інпута та контейнера
 function destroyBoxes() {
   input.value = '';
   boxesContainer.innerHTML = '';
 }
 
+//  додаємо слухача події на кнопку очищення
 destroyBtn.addEventListener('click', destroyBoxes);
