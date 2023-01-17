f5381987981159bde64cb75f2f97b815c438d2b8
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-01-17 11:24:08 +0200

    fix function getRandomHexColor

--- js/task-10.js
+++ js/task-10.js
@@ -15,7 +15,9 @@ input.addEventListener('input', function () {
 });
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 // колбек-функція для слухача події кнопки createBtn
